499ceaa72b0d35908476ea05fb33f4ce8ad3047c
张小刚 <2767577447@qq.com>
2016-05-13 15:38:05 +0800

    偏好

---
+++ GitStatus/controller/PreferenceWindowController.h
@@ -0,0 +1,13 @@
+//
+//  PreferenceWindowController.h
+//  GitStatus
+//
+//  Created by 张小刚 on 16/5/13.
+//  Copyright © 2016年 lyeah company. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PreferenceWindowController : NSWindowController
+
+@end
---
+++ GitStatus/controller/PreferenceWindowController.m
@@ -0,0 +1,23 @@
+//
+//  PreferenceWindowController.m
+//  GitStatus
+//
+//  Created by 张小刚 on 16/5/13.
+//  Copyright © 2016年 lyeah company. All rights reserved.
+//
+
+#import "PreferenceWindowController.h"
+
+@interface PreferenceWindowController ()
+
+@end
+
+@implementation PreferenceWindowController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
